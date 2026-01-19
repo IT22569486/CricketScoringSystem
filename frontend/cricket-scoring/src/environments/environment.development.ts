cf9d42a2801dedd6b9d9f08a57d8c4ec4e021b58
IT22569486 <it22569486@my.sliit.lk>
2026-01-19 22:58:05 +0530

    feat: add Angular configuration and proxy setup

---
+++ frontend/cricket-scoring/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7191/api',
+  firebase: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID'
+  }
+};
